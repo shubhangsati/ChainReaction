01c05a2a663f29c35007b304609e0828e2a7f743
Shubhang Sati <shubhang16198@iiitd.ac.in>
2017-11-17 08:43:25 +0530

    .

--- src/application/Grid.java
+++ src/application/Grid.java
@@ -1,7 +1,16 @@
 package application;
 
+import Game.SplitTimeline;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.EventHandler;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +18,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.util.Duration;
 //import javafx.scene.control.Button;
 //import javafx.scene.layout.GridPane;
 //import javafx.scene.layout.StackPane;
@@ -23,9 +33,10 @@ public class Grid extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader root = new FXMLLoader(getClass().getResource("Grid.fxml"));
-		Scene scene = new Scene(root.load());
-		GridController grid = root.getController();
+		Scene scene = new Scene(root.load(), 335, 600, true);
+        GridController grid = root.getController();
 		GridPane top = grid.topGrid;
+		top.setTranslateZ(-10);
 
 		top.getChildren().forEach(item -> {
 		    item.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -36,6 +47,13 @@ public class Grid extends Application {
                     if (row == null) row = 0;
                     if (column == null) column = 0;
                     System.out.println(row + " " + column);
+                    Sphere c = new Sphere(15);
+                    c.setTranslateX(item.getLayoutX() + 5);
+                    c.setTranslateY(item.getLayoutY() - 5);
+                    c.setTranslateZ(10);
+                    top.getChildren().add(c);
+                    System.out.println(top.getTranslateZ());
+                    System.out.println(c.getTranslateZ());
                 }
             });
         });
